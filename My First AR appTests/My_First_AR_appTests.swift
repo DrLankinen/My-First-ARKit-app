c062c23ec0447d0fa34b777ff83bba0bb16a336d
Elias Lankinen <elias@lankinen.xyz>
2020-08-29 09:05:55 +0300

    Initial Commit

---
+++ My First AR appTests/My_First_AR_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  My_First_AR_appTests.swift
+//  My First AR appTests
+//
+//  Created by Elias Lankinen on 8/29/20.
+//  Copyright © 2020 Elias Lankinen. All rights reserved.
+//
+
+import XCTest
+@testable import My_First_AR_app
+
+class My_First_AR_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
